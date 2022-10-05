b84e4bb0e0891d3d356ebd2107f9a83fe4006b52
Anatoly Lushnikov <anatoly.lushnikov@aurea.com>
2022-10-05 20:04:04 +0400

    Update run-mysql.sh

--- api/run-mysql.sh
+++ api/run-mysql.sh
@@ -1,7 +1,6 @@
 docker run --name mysql -d \
     -p 3306:3306 \
     -e MYSQL_ROOT_PASSWORD=123456 \
-    -e MYSQL_USER=root \
     -e MYSQL_DATABASE=testdb \
     --restart unless-stopped \
     mysql
